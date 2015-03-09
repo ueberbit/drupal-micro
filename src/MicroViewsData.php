299142883595b756478a65751d21cbd5edf7b429
Florian Weber <florian.weber@ueberbit.de>
2015-03-09 13:32:40 +0100

    Use default views integration from core.

--- src/MicroViewsData.php
+++ src/MicroViewsData.php
@@ -7,124 +7,17 @@
 
 namespace Drupal\micro;
 
+use Drupal\views\EntityViewsData;
 use Drupal\views\EntityViewsDataInterface;
 
 /**
  * Provides views data for the micro entity type.
  */
 
-class MicroViewsData implements EntityViewsDataInterface {
+class MicroViewsData extends EntityViewsData implements EntityViewsDataInterface {
 
-  /**
-   * {@inheritdoc}
-   */
   public function getViewsData() {
-    $data = array();
-
-    $data['micro']['table']['group']  = t('Micro');
-    $data['micro']['table']['base'] = array(
-      'field' => 'mid',
-      'title' => t('Micro'),
-      'help' => '@todo',
-    );
-    $data['micro']['table']['entity type'] = 'micro';
-    $data['micro']['table']['wizard_id'] = 'micro';
-
-    // mid field
-    $data['micro']['mid'] = array(
-      'title' => t('Micro ID'),
-      'help' => t('The mid of a micro entity.'),
-      'field' => array(
-        'id' => 'numeric',
-      ),
-      'argument' => array(
-        'id' => 'standard',
-      ),
-      'sort' => array(
-        'id' => 'standard',
-      ),
-    );
-
-    // type field
-    $data['micro']['type'] = array(
-      'title' => t('Type'),
-      'help' => t('The micro type.'),
-      /*
-      'field' => array(
-        'id' => 'micro_type',
-      ),
-      */
-      'sort' => array(
-        'id' => 'standard',
-      ),
-      'filter' => array(
-        'id' => 'bundle',
-      ),
-      /*
-      'argument' => array(
-        'id' => 'micro_type',
-      ),
-      */
-    );
-
-    $data['micro_field_data']['table']['group']  = t('Micro');
-    $data['micro_field_data']['table']['join']['micro'] = [
-      'left_field' => 'mid',
-      'field' => 'mid',
-    ];
-
-    // Term title field
-    $data['micro_field_data']['title'] = array(
-      'title' => t('Title'),
-      'help' => t('The micro title.'),
-      'field' => array(
-        'id' => 'standard',
-      ),
-      'sort' => array(
-        'id' => 'standard',
-      ),
-      'filter' => array(
-        'id' => 'string',
-        'help' => t('Micro title.'),
-      ),
-      'argument' => array(
-        'id' => 'string',
-        'help' => t('Micro title.'),
-        'many to one' => TRUE,
-        'empty field title' => t('Uncategorized'),
-      ),
-    );
-
-    $data['micro_field_data']['langcode'] = array(
-      'title' => t('Translation language'),
-      'help' => t('The language of the micro or translation.'),
-      'field' => array(
-        'id' => 'standard',
-      ),
-      'filter' => array(
-        'id' => 'language',
-      ),
-      'argument' => array(
-        'id' => 'language',
-      ),
-      'sort' => array(
-        'id' => 'standard',
-      ),
-    );
-
-    $data['micro_field_data']['changed'] = array(
-      'title' => t('Updated date'),
-      'help' => t('The date the content was last updated.'),
-      'field' => array(
-        'id' => 'date',
-      ),
-      'sort' => array(
-        'id' => 'date'
-      ),
-      'filter' => array(
-        'id' => 'date',
-      ),
-    );
+    $data = parent::getViewsData();
 
     $data['micro']['view_micro'] = array(
       'field' => array(
