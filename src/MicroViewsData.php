61f7771613a30ee9ef9712defc1df9336cd9af5a
Florian Weber <florian.weber@ueberbit.de>
2015-07-02 00:28:34 +0200

    Remove custom views integration link handles from entity_view module.

--- src/MicroViewsData.php
+++ src/MicroViewsData.php
@@ -13,36 +13,10 @@ use Drupal\views\EntityViewsDataInterface;
 /**
  * Provides views data for the micro entity type.
  */
-
-class MicroViewsData extends EntityViewsData implements EntityViewsDataInterface {
+class MicroViewsData extends EntityViewsData {
 
   public function getViewsData() {
     $data = parent::getViewsData();
-
-    $data['micro']['view_micro'] = array(
-      'field' => array(
-        'title' => t('Link to content'),
-        'help' => t('Provide a simple link to the content.'),
-        'id' => 'entity_link',
-      ),
-    );
-
-    $data['micro']['edit_micro'] = array(
-      'field' => array(
-        'title' => t('Link to edit content'),
-        'help' => t('Provide a simple link to edit the content.'),
-        'id' => 'entity_link_edit',
-      ),
-    );
-
-    $data['micro']['delete_micro'] = array(
-      'field' => array(
-        'title' => t('Link to delete content'),
-        'help' => t('Provide a simple link to delete the content.'),
-        'id' => 'entity_link_delete',
-      ),
-    );
-
     return $data;
   }
 
